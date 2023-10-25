5604d451462131073150be1d79a1298c40442d2e
Boothu <144676806+Boothu@users.noreply.github.com>
2023-10-25 11:10:47 +0100

    Integrated localStorage
    Integrated localStorage so that name is stored

--- F1HistoryJS.js
+++ F1HistoryJS.js
@@ -2,11 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var currentPage = window.location.href;
     if (currentPage.includes("F1HistoryHomepage.html")) {
-        var userName = prompt("Please enter your name:");
-
-        if (userName) {
+        var name = localStorage.getItem('name');
+        if (name == "") {
+            var userName = prompt("Please enter your name:");
+            localStorage.setItem('name', userName)
             var welcome = document.getElementById("welcome");
-            welcome.innerHTML = "Welcome to F1 History, " + userName + "!";
+            welcome.innerHTML = "Welcome to F1 History, " + name + "!";
+        }
+        else {
+            var welcome = document.getElementById("welcome");
+            welcome.innerHTML = "Welcome to F1 History, " + name + "!";
         }
     }
 });
