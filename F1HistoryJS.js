b5fc90baf294e506b67190c1d9062eb4d8d67489
Boothu <144676806+Boothu@users.noreply.github.com>
2023-11-07 14:19:34 +0000

    Fixed some problems with name being stored in localStorage

--- F1HistoryJS.js
+++ F1HistoryJS.js
@@ -3,15 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     var currentPage = window.location.href;
     if (currentPage.includes("F1HistoryHomepage.html")) {
         var name = localStorage.getItem('name');
-        if (name == "") {
+        if (name == "" || name == null) {
             var userName = prompt("Please enter your name:");
             localStorage.setItem('name', userName)
             var welcome = document.getElementById("welcome");
             welcome.innerHTML = "Welcome to F1 History, " + name + "!";
+            location.reload();
         }
         else {
             var welcome = document.getElementById("welcome");
             welcome.innerHTML = "Welcome to F1 History, " + name + "!";
+            // localStorage.clear();
+            // Used for testing purposes
         }
     }
 });
