fcae9dc729af5a95b0ef32ad49ff0f54936970a5
Boothu <144676806+Boothu@users.noreply.github.com>
2023-11-13 14:35:15 +0000

    Utilized function

--- script.ts
+++ script.ts
@@ -7,3 +7,5 @@ function giveName (drivers: Drivers): string {
     return 'This drivers name is ${person.forename} ${person.surname}';
 }
 
+let lh = {forename: "Lewis", surname: "Hamilton"};
+console.log(giveName(lh));  
